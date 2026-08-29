83d2d2befe93631753167642f766ba38fdc02840
Msuf123 <akshatmalik18t@gmail.com>
2026-08-29 21:41:49 +0200

    Completed the game but with a error

--- SimpleDotCom.java
+++ SimpleDotCom.java
@@ -1,10 +1,9 @@
 public class SimpleDotCom{
-	int[] locationCells;
-	int numOfHits=0;
+	
 	public static void main(String[] args){
 	int numOfGuesses=0;
 	GameHelper helper=new GameHelper();
-	SimpleStartUp theStartup=new SimpleStartup();
+	SimpleStartup theStartup=new SimpleStartup();
 	int randomNum=(int)(Math.random()*5);
 	int[] locations={randomNum,randomNum+1,randomNum+2};
 	theStartup.setLocationCells(locations);
@@ -19,23 +18,5 @@ public class SimpleDotCom{
 	}
 	}
 	}
-	public void setLocationCells(int[] locs){
-		locationCells=locs;
-	}
-	public String checkYourself(String stringGuess){
-		int guess=Integer.parseInt(stringGuess);
-		String result="miss";
-		for(int cell:locationCells){
-			if(guess==cell){
-				result="hit";
-				numOfHits++;
-				break;
-			}
-		}
-		if(numOfHits==locationCells.length){
-			result="kill";
-		}
-		System.out.println(result);
-		return result;
-	}
+
 }
